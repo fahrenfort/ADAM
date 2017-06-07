9cbb847544e91da9c6784c25569a50660b4b5c7a
Johannes Fahrenfort <fahrenfort.work@gmail.com>
2017-06-07 16:09:20 +0200

    rename

--- eeg_mvpa/compute_group_ERP_or_TFR.m
+++ eeg_mvpa/compute_group_ERP_or_TFR.m
@@ -1,4 +1,4 @@
-function group_FT_EEG = compute_group_ERP2(folder_name,cfg)
+function group_FT_EEG = compute_group_ERP_or_TFR(folder_name,cfg)
 % Get all the ERPs from settings and put them into a cell array for further
 % analysis in fieldtrip
 %
@@ -32,14 +32,14 @@ if isempty(folder_name)
     end
     % loop through directories
     for cdirz = 1:numel(dirz)
-        group_FT_EEG{cdirz} = subcompute_group_ERP([folder_name filesep dirz{cdirz}],cfg);
+        group_FT_EEG{cdirz} = subcompute_group([folder_name filesep dirz{cdirz}],cfg);
     end
     cfg.folder = folder_name;
 else
     if ~exist('folder_name','dir') && ~iscell(folder_name) 
         error([folder_name ' should refer to a full and existing folder path. Alternatively leave folder_name empty to pop up a selection dialog.']);
     end
-    group_FT_EEG  = subcompute_group_ERP(folder_name,cfg);
+    group_FT_EEG  = subcompute_group(folder_name,cfg);
 end
 
 % fill cfg.plot_order in case not given by user and from single folder
@@ -47,13 +47,20 @@ if numel(cfg.plot_order) == 1
     cfg.plot_order = {stats(:).condname};
 end
 
-function group_FT_EEG = subcompute_group_ERP(folder_name,cfg)
+function group_FT_EEG = subcompute_group(folder_name,cfg)
 channelpool = 'ALL';
 % unpack graphsettings
 v2struct(cfg);
 
 % get filenames
-plotFreq = ''; % this is empty for now, but might be used to look at the ERPs obtained from a TF analysis
+freqfolder_contains_freq_time = ~isempty(dir([folder_name filesep channelpool filesep 'allfreqs']));
+if freqfolder_contains_freq_time
+    datafield = 'TFR';
+    plotFreq = [filesep 'allfreqs'];
+else
+    datafield = 'ERP';
+    plotFreq = '';
+end
 cfg.plotFreq = plotFreq;
 subjectfiles = dir([folder_name filesep channelpool plotFreq filesep '*.mat']);
 [~, condname] = fileparts(folder_name);
@@ -65,29 +72,23 @@ if nSubj == 0
     error(['cannot find data in specified folder ' folder_name filesep channelpool plotFreq filesep]);
 end
 
-% do the loop, restrict time and frequency if applicable
+% do the loop
 for cSubj = 1:nSubj
     fprintf(1,'loading subject %d of %d\n', cSubj, nSubj);
     matObj = matfile([folder_name filesep channelpool plotFreq filesep subjectfiles{cSubj}]);
-    if ~isempty(whos(matObj,'settings'))
-        settings = matObj.settings;
-    else % BW compatibility, will become obsolete over time
-        error('Cannot find settings struct which the contains the ERP info');
-    end
-    % OLD for backwards compatible
-    if isfield(settings,'ft_erpstruct')
-        FT_ERP = settings.ft_erpstruct;
-    elseif isfield(settings,'FT_ERP')
-        FT_ERP = settings.FT_ERP;
+    
+    % get goodies
+    if strcmpi(datafield,'ERP')
+        FT_EEG = matObj.FT_ERP;
     else
-        error('no ERP was computed for these data');
+        FT_EEG = matObj.FT_TFR;
     end
     
-    if iscell(FT_ERP)
-        FT_ERP = FT_ERP{2};
-        disp('Extracting ERPs from testing data');
+    if iscell(FT_EEG)
+        FT_EEG = FT_EEG{2};
+        disp(['Extracting ' datafield 's from testing data']);
     end
-    group_FT_EEG{cSubj} = FT_ERP;
+    group_FT_EEG{cSubj} = FT_EEG;
     
 end
 
