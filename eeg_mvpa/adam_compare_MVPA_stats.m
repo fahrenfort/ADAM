5c168a1bd7da5734c94781f038c1f348528eb0cb
Johannes Fahrenfort <fahrenfort.work@gmail.com>
2018-01-09 13:29:04 +0100

    updates

--- eeg_mvpa/adam_compare_MVPA_stats.m
+++ eeg_mvpa/adam_compare_MVPA_stats.m
@@ -1,19 +1,32 @@
-function stats = adam_compare_MVPA_stats(stats1,stats2,cfg,mask)
-% function stats = compare_MVPA_stats(stats1,stats2,cfg,mask)
+function stats = adam_compare_MVPA_stats(cfg,stats1,stats2,mask)
+% function stats = compare_MVPA_stats(cfg,stats1,stats2,mask)
 % compares and subtracts stats for plotting: stats1 - stats2
 % cfg determines how statistics are computed
-% cfg.one_two_tailed = 'two' (can also be 'one')
+% cfg.tail = 'both' (can also be 'right' or 'left')
 % cfg.indiv_pval = .05;
 % cfg.cluster_pval = .05;
 % cfg.mpcompcor_method = 'uncorrected' (default, can also be 'cluster_based', 'fdr' or 'none')
 
-one_two_tailed = 'two';
+% get some defaults
+reduce_dims = '';
+tail = 'both';
 cluster_pval = .05;
 indiv_pval = .05;
 mpcompcor_method = 'uncorrected';
+% unpack original cfg
+if isfield(stats1,'cfg')
+    v2struct(stats1.cfg,{'fieldNames','reduce_dims','tail','cluster_pval','indiv_pval','mpcompcor_method','trainlim','testlim','reduce_dims'});
+elseif isfiled(stats2,'cfg')
+    v2struct(stats2.cfg,{'fieldNames','reduce_dims','tail','cluster_pval','indiv_pval','mpcompcor_method','trainlim','testlim','reduce_dims'});
+end
 v2struct(cfg);
-pval(1) = indiv_pval;
-pval(2) = cluster_pval;
+if exist('one_two_tailed','var')
+    error('The cfg.one_two_tailed field has been replaced by the cfg.tail field. Please replace cfg.one_two_tailed with cfg.tail using ''both'', ''left'' or ''right''. See help for further info.');
+end
+
+% pack cfg with defaults
+nameOfStruct2Update = 'cfg';
+cfg = v2struct(reduce_dims,tail,cluster_pval,indiv_pval,tail,mpcompcor_method,trainlim,testlim,reduce_dims,nameOfStruct2Update);
 
 % compute some values
 ClassTotal{1} = stats1.indivClassOverTime;
@@ -21,35 +34,27 @@ ClassTotal{2} = stats2.indivClassOverTime;
 nSubj = size(ClassTotal{1},1);
 stats.ClassOverTime = squeeze(mean(ClassTotal{1}-ClassTotal{2}))';
 stats.StdError = squeeze(std(ClassTotal{1}-ClassTotal{2})/sqrt(nSubj))';
-stats.condname = [stats1.condname '-' stats2.condname];
+stats.condname = [stats1.condname ' - ' stats2.condname];
 settings = stats1.settings; % assuming these are the same!
+settings.measuremethod = 'accuracy difference';
 
 % statistical testing
 if nargin < 4
 	mask = ones([size(ClassTotal{1},2) size(ClassTotal{1},3)]);
 end
     
-if strcmp(mpcompcor_method,'fdr')
+if strcmpi(mpcompcor_method,'fdr')
     % FDR CORRECTION
-    if strcmp(one_two_tailed,'two')
-        [~,ClassPvals] = ttest(ClassTotal{1},ClassTotal{2},'tail','both');
-    else
-        [~,ClassPvals] = ttest(ClassTotal{1},ClassTotal{2},'tail','right');
-    end
-    ClassPvals = squeeze(ClassPvals);
-    h = fdr_bh(ClassPvals,pval(2),'pdep');
-    ClassPvals(~h) = 1;
+    [~,ClassPvals] = ttest(ClassTotal{1},ClassTotal{2},indiv_pval,'tail',tail);
+    thresh = fdr(squeeze(ClassPvals),cluster_pval);
+    ClassPvals(ClassPvals>thresh) = 1;
     pStruct = []; % still need to implement
-elseif strcmp(mpcompcor_method,'cluster_based')
+elseif strcmpi(mpcompcor_method,'cluster_based')
     % CLUSTER BASED CORRECTION
     [ ClassPvals, pStruct ] = cluster_based_permutation(ClassTotal{1},ClassTotal{2},cfg,settings,mask);
-elseif strcmp(mpcompcor_method,'uncorrected')
+elseif strcmpi(mpcompcor_method,'uncorrected')
     % NO MP CORRECTION
-    if strcmp(one_two_tailed,'two')
-        [~,ClassPvals] = ttest(ClassTotal{1},ClassTotal{2},'tail','both');
-    else
-        [~,ClassPvals] = ttest(ClassTotal{1},ClassTotal{2},'tail','right');
-    end
+    [~,ClassPvals] = ttest(ClassTotal{1},ClassTotal{2},'tail',tail);
     ClassPvals = squeeze(ClassPvals);
     ClassPvals(~mask) = 1;
     pStruct = []; % still need to implement
@@ -62,4 +67,5 @@ end
 % output stats
 stats.pVals = ClassPvals;
 stats.pStruct = pStruct;
+stats.cfg = cfg;
 stats.settings = settings;
